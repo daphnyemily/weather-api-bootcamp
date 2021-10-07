785beb200d335337e3bbdca6ff9eb8f25ac62dde
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-07 19:11:47 -0400

    fixed API issues, added CSS

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@
 
 document.querySelector('button').addEventListener('click', searchButton)
 
+
 function searchButton(){
-    const API_KEY = 'a4f72bc407c3457eb79174947213009'
     let city = document.querySelector('input').value
-    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}united states&aqi=no`
+    const url = `http://api.weatherapi.com/v1/current.json?key=7cefcbd3fb7f4eeb90312835210710&q=${city}`
 
     fetch(url)
         .then(res => res.json())
         .then(data => { 
             console.log(data)
-            // document.querySelector('h3').innerText = data.location.country
-            // document.querySelector('h2').innerText = data.location.name
+            document.querySelector('#city').innerText = data.location.name
+            document.querySelector('#state').innerText = data.location.region
+            document.querySelector('h3').innerText = data.location.country
             document.querySelector('#tempF').innerText = data.current.temp_f    
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
-}
 
+    }
